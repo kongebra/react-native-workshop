216d7f7195de0da6b2795f4f7304f4f718aa76ab
saru <sarujans.dev@gmail.com>
2019-09-05 14:26:30 +0200

    Initializing firebase

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
