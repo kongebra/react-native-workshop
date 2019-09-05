0a306dd8a30945cba75ff4b90f0af0754ed01445
saru <sarujans.dev@gmail.com>
2019-09-05 14:23:42 +0200

    created config.js for firbase config

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6n5QmT0KrZISb4iWbU2e6-G4REtvEihI",
+  authDomain: "react-native-workshop-e9569.firebaseapp.com",
+  databaseURL: "https://react-native-workshop-e9569.firebaseio.com",
+  projectId: "react-native-workshop-e9569",
+  storageBucket: "react-native-workshop-e9569.appspot.com",
+  messagingSenderId: "390634288073",
+  appId: "1:390634288073:web:268c8a3e68ce9427d6ee18"
+};
