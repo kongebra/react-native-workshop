fb575aad9e7cef85540ea631b602564abe8561bf
Svein Are Danielsen <svein.danielsen@inmeta.no>
2019-09-05 13:47:03 +0200

    added api urls

---
+++ api-config.js
@@ -0,0 +1,14 @@
+export const API_URL = {
+    SEARCH_BY_NAME: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=",
+    LIST_ALL_BY_FIRST_LETTER:
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=",
+    SEARCH_INGREDIENT_BY_NAME:
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=",
+    FULL_DETAIL_BY_ID:
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=",
+    INGREDIENT_BY_ID:
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=",
+    RANDOM_COCKTAIL: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+    SEARCH_BY_INGREDIENT:
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+};
