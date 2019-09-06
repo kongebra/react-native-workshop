593de00cb61f56b03b06a392ab742ec0627ae7e0
saru <sarujans.dev@gmail.com>
2019-09-06 10:05:35 +0200

    Created stylesheet

--- app/screens/cocktail-home.js
+++ app/screens/cocktail-home.js
@@ -3,130 +3,129 @@ import { useObserver } from "mobx-react-lite";
 import { useNavigation } from "react-navigation-hooks";
 import { cocktailContext } from "../providers/cocktail-context";
 
-import { Dimensions, SafeAreaView, Image } from "react-native";
+import { StyleSheet, Dimensions, SafeAreaView, Image } from "react-native";
 import {
-    Container,
-    Content,
-    Header,
-    View,
-    DeckSwiper,
-    Card,
-    CardItem,
-    Thumbnail,
-    Text,
-    Left,
-    Right,
-    Body,
-    Title,
-    Icon,
-    Button,
-    Footer,
-    FooterTab,
-    Fab
+  Container,
+  Content,
+  Header,
+  View,
+  DeckSwiper,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Left,
+  Right,
+  Body,
+  Title,
+  Icon,
+  Button,
+  Footer,
+  FooterTab,
+  Fab
 } from "native-base";
 
 export default function CocktailHome() {
-    const store = useContext(cocktailContext);
-    const [current, setCurrent] = useState({});
+  const store = useContext(cocktailContext);
+  const [current, setCurrent] = useState({});
 
-    const { navigate } = useNavigation();
+  const { navigate } = useNavigation();
 
-    useEffect(() => {
-        populateItems();
-    }, []);
+  useEffect(() => {
+    populateItems();
+  }, []);
 
-    const populateItems = async () => {
-        await store.getRandom();
-    };
+  const populateItems = async () => {
+    await store.getRandom();
+  };
 
-    const handleSwipeRight = () => {
-        console.log("RIGHT", current.strDrink);
-    };
+  const handleSwipeRight = () => {
+    console.log("RIGHT", current.strDrink);
+  };
 
-    const handleSwipeLeft = () => {
-        console.log("LEFT", current.strDrink);
-    };
+  const handleSwipeLeft = () => {
+    console.log("LEFT", current.strDrink);
+  };
 
-    return useObserver(() => (
-        <SafeAreaView>
-            <Container>
-                <Header>
-                    <Left>
-                        <Button transparent>
-                            <Icon name="menu" />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Header</Title>
-                    </Body>
-                    <Right />
-                </Header>
-                <Content>
-                    {store.isLoading ? (
-                        <Text>Loading...</Text>
-                    ) : (
-                        <>
-                            <View>
-                                <DeckSwiper
-                                    ref={c => (this._deckSwiper = c)}
-                                    dataSource={store.cocktails}
-                                    looping={false}
-                                    renderEmpty={() => (
-                                        <View style={{ alignSelf: "center" }}>
-                                            <Text>Over</Text>
+  return useObserver(() => (
+    <SafeAreaView>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
 
-                                            <Button
-                                                onPress={() => {
-                                                    populateItems();
-                                                }}
-                                            >
-                                                <Text>Load more...</Text>
-                                            </Button>
-                                        </View>
-                                    )}
-                                    renderItem={item => {
-                                        setCurrent(item);
+        {store.isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <>
+            <View>
+              <DeckSwiper
+                ref={c => (this._deckSwiper = c)}
+                dataSource={store.cocktails}
+                looping={false}
+                renderEmpty={() => (
+                  <View style={styles.viewStyle}>
+                    <Text>Over</Text>
 
-                                        return (
-                                            <Card style={{ elevation: 3 }}>
-                                                <CardItem>
-                                                    <Body>
-                                                        <Text>
-                                                            {item.strDrink}
-                                                        </Text>
-                                                        <Text note>
-                                                            {item.strCategory}
-                                                        </Text>
-                                                    </Body>
-                                                </CardItem>
-                                                <CardItem cardBody>
-                                                    <Image
-                                                        style={{
-                                                            height: Math.round(
-                                                                Dimensions.get(
-                                                                    "window"
-                                                                ).height *
-                                                                    0.66666
-                                                            ),
-                                                            flex: 1
-                                                        }}
-                                                        source={{
-                                                            uri:
-                                                                item.strDrinkThumb
-                                                        }}
-                                                    />
-                                                </CardItem>
-                                            </Card>
-                                        );
-                                    }}
-                                    onSwipeRight={handleSwipeRight}
-                                    onSwipeLeft={handleSwipeLeft}
-                                />
-                            </View>
-                        </>
-                    )}
-                </Content>
-            </Container>
-        </SafeAreaView>
-    ));
+                    <Button
+                      onPress={() => {
+                        populateItems();
+                      }}
+                    >
+                      <Text>Load more...</Text>
+                    </Button>
+                  </View>
+                )}
+                renderItem={item => {
+                  setCurrent(item);
+
+                  return (
+                    <Card style={styles.cardStyle}>
+                      <CardItem>
+                        <Body>
+                          <Text>{item.strDrink}</Text>
+                          <Text note>{item.strCategory}</Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem cardBody>
+                        <Image
+                          style={styles.imageStyle}
+                          source={{
+                            uri: item.strDrinkThumb
+                          }}
+                        />
+                      </CardItem>
+                    </Card>
+                  );
+                }}
+                onSwipeRight={handleSwipeRight}
+                onSwipeLeft={handleSwipeLeft}
+              />
+            </View>
+          </>
+        )}
+      </Container>
+    </SafeAreaView>
+  ));
 }
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    alignSelf: "center"
+  },
+  cardStyle: {
+    elevation: 3
+  },
+  imageStyle: {
+    height: Math.round(Dimensions.get("window").height * 0.66666),
+    flex: 1
+  }
+});
